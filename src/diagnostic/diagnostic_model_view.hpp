352441aee7eec8700d4a6f55c20b934e0cc5d753
UCaromel <caromelulysse@gmail.com>
2025-04-10 16:26:06 +0200

    BROKEN: compile error with the MHDDiagnosticWriter

--- src/diagnostic/diagnostic_model_view.hpp
+++ src/diagnostic/diagnostic_model_view.hpp
@@ -8,6 +8,7 @@
 #include "amr/physical_models/mhd_model.hpp"
 
 #include "cppdict/include/dict.hpp"
+#include <type_traits>
 
 #include <type_traits>
 
@@ -38,13 +39,6 @@ public:
         , hierarchy_{hierarchy}
     {
     }
-
-    NO_DISCARD VecField& getB() const { return model_.state.electromag.B; }
-
-    NO_DISCARD VecField& getE() const { return model_.state.electromag.E; }
-
-    NO_DISCARD auto& getIons() const { return model_.state.ions; }
-
 
     template<typename Action>
     void visitHierarchy(Action&& action, int minLevel = 0, int maxLevel = 0)
@@ -102,6 +96,115 @@ protected:
     Hierarchy& hierarchy_;
 };
 
+// we need to check the model type or the template specialization from the typelist will fail to
+// compile for the other model.
+template<typename Model>
+struct is_hybrid_model : std::false_type
+{
+};
+
+template<typename... Args>
+struct is_hybrid_model<solver::HybridModel<Args...>> : std::true_type
+{
+};
+
+template<typename Model>
+struct is_mhd_model : std::false_type
+{
+};
+
+template<typename... Args>
+struct is_mhd_model<solver::MHDModel<Args...>> : std::true_type
+{
+};
+
+template<typename HybridModel>
+bool constexpr is_valid_hybrid_model = []() {
+    if constexpr (is_hybrid_model<HybridModel>::value)
+    {
+        return std::is_same_v<solver::type_list_to_hybrid_model_t<typename HybridModel::type_list>,
+                              HybridModel>;
+    }
+    else
+    {
+        return false;
+    }
+}();
+
+template<typename MHDModel>
+bool constexpr is_valid_mhd_model = []() {
+    if constexpr (is_mhd_model<MHDModel>::value)
+    {
+        return std::is_same_v<solver::type_list_to_mhd_model_t<typename MHDModel::type_list>,
+                              MHDModel>;
+    }
+    else
+    {
+        return false;
+    }
+}();
+
+struct HybridIdentifier
+{
+};
+struct MHDIdentifier
+{
+};
+
+template<typename Model>
+using ModelIdentifier
+    = std::conditional_t<is_valid_hybrid_model<Model>, HybridIdentifier,
+                         std::conditional_t<is_valid_mhd_model<Model>, MHDIdentifier, void>>;
+
+template<typename Hierarchy, typename Model, typename = ModelIdentifier<Model>>
+class ModelView
+{
+    static_assert(is_valid_hybrid_model<Model> || is_valid_mhd_model<Model>,
+                  "Model must be either a valid HybridModel or MHDModel.");
+};
+
+template<typename Hierarchy, typename Model>
+class ModelView<Hierarchy, Model, HybridIdentifier> : public BaseModelView<Hierarchy, Model>
+{
+    using VecField = typename Model::vecfield_type;
+
+public:
+    using Identifier = HybridIdentifier;
+
+    using BaseModelView<Hierarchy, Model>::BaseModelView;
+
+    NO_DISCARD VecField& getB() const { return this->model_.state.electromag.B; }
+
+    NO_DISCARD VecField& getE() const { return this->model_.state.electromag.E; }
+
+    NO_DISCARD auto& getIons() const { return this->model_.state.ions; }
+};
+
+template<typename Hierarchy, typename Model>
+class ModelView<Hierarchy, Model, MHDIdentifier> : public BaseModelView<Hierarchy, Model>
+{
+    using Field    = typename Model::field_type;
+    using VecField = typename Model::vecfield_type;
+
+public:
+    using Identifier = MHDIdentifier;
+
+    using BaseModelView<Hierarchy, Model>::BaseModelView;
+
+    NO_DISCARD Field& getRho() const { return this->model_.state.rho; }
+
+    NO_DISCARD VecField& getV() const { return this->model_.state.V; }
+
+    NO_DISCARD VecField& getB() const { return this->model_.state.B; }
+
+    NO_DISCARD Field& getP() const { return this->model_.state.P; }
+
+    NO_DISCARD VecField& getRhoV() const { return this->model_.state.rhoV; }
+
+    NO_DISCARD VecField& getEtot() const { return this->model_.state.Etot; }
+
+    // What about E, J, etc. ? Those 2 in particular are physically relevant temporaries.
+};
 
 template<typename Hierarchy, typename Model, typename Enable = void>
 class ModelView;
