d186775407fffcdf1b2f272b8a23d6f53270b6f1
UCaromel <caromelulysse@gmail.com>
2025-12-18 18:13:36 +0100

    3d testing

--- src/amr/data/particles/refine/split.hpp
+++ src/amr/data/particles/refine/split.hpp
@@ -3,6 +3,7 @@
 
 #include "amr/data/particles/refine/split_1d.hpp" // IWYU pragma: keep
 #include "amr/data/particles/refine/split_2d.hpp" // IWYU pragma: keep
+#include "amr/data/particles/refine/split_3d.hpp" // IWYU pragma: keep
 
 
 #endif // endif PHARE_SPLIT_HPP
