2277ef037abe9cc4a928e05a0ce65e0716b3c13e
UCaromel <caromelulysse@gmail.com>
2026-03-10 14:16:59 +0100

    wip 2

--- src/core/numerics/energy_corrector/energy_corrector.hpp
+++ src/core/numerics/energy_corrector/energy_corrector.hpp
@@ -17,7 +17,7 @@ class EnergyCorrector : public LayoutHolder<GridLayout>
 
 public:
     template<typename Field, typename VecField>
-    void operator()(Field& Etot, VecField const& B, VecField const& Bc)
+    void operator()(Field& Etot, VecField const& B, VecField const& Bc) const
     {
         if (!this->hasLayout())
             throw std::runtime_error(
@@ -41,7 +41,7 @@ public:
     template<typename Field, typename VecField>
     void operator()(Field& Etot, VecField const& B, VecField const& Bc) const
     {
-        layout_.evalOnBox(Etot, [&](auto&... args) mutable { correct_(Etot, B, Bc, args...); });
+        layout_.evalOnBox(Etot, [&](auto&... args) mutable { correct_(Etot, B, Bc, {args...}); });
     }
 
 
@@ -49,16 +49,17 @@ private:
     GridLayout layout_;
 
 
-    void correct_(auto& Etot, auto const& B, auto const& Bc, MeshIndex<dimension> index)
+    void correct_(auto& Etot, auto const& B, auto const& Bc, MeshIndex<dimension> index) const
     {
         auto const& [Bx, By, Bz]    = B();
         auto const& [Bxc, Byc, Bzc] = Bc();
 
-        auto const Bxf = GridLayout::project(Bx, index, GridLayout::faceXToCellCenter);
-        auto const Byf = GridLayout::project(By, index, GridLayout::faceYToCellCenter);
-        auto const Bzf = GridLayout::project(Bz, index, GridLayout::faceZToCellCenter);
+        auto const Bxf = GridLayout::project(Bx, index, GridLayout::faceXToCellCenter());
+        auto const Byf = GridLayout::project(By, index, GridLayout::faceYToCellCenter());
+        auto const Bzf = GridLayout::project(Bz, index, GridLayout::faceZToCellCenter());
 
-        auto const B2c = Bxc * Bxc + Byc * Byc + Bzc * Bzc;
+        auto const B2c
+            = Bxc(index) * Bxc(index) + Byc(index) * Byc(index) + Bzc(index) * Bzc(index);
         auto const B2f = Bxf * Bxf + Byf * Byf + Bzf * Bzf;
         Etot(index) += B2f - B2c;
     }
